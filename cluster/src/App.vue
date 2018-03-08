53a23b6948958ec8883c93b9ef2e64899aa246c7
Charlie Furniss <charliefurniss96@gmail.com>
2018-03-08 20:59:50 +0000

    basic styling

--- cluster/src/App.vue
+++ cluster/src/App.vue
@@ -10,7 +10,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 html, body {
   height: 100%;
   width: 100%;
@@ -22,5 +22,21 @@ html, body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  header {
+    &.mdl-layout__header {
+      @media screen and (max-width: 1024px) {
+        display: flex;
+      }
+    }
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex: 1 1 auto;
+  }
 }
 </style>
--- cluster/src/components/InitialView.vue
+++ cluster/src/components/InitialView.vue
@@ -1,10 +1,34 @@
 <template>
-  <div class="content">
-    <div v-for="data in data" v-bind:key="data.id">
-      <p>{{data.Name}}</p>
-    </div>
+  <div class="mdl-layout mdl-js-layout">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout-icon"></div>
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout__title">Cluster search</span>
+      </div>
+    </header>
+    <main class="content">
+      <section class="search">
+        
+      </section>
+      <section class="results">
+        <div class="result" v-for="data in data" v-bind:key="data.id">
+          <div class="result__item">
+            <span class="result__item-label">File path</span>
+            <span class="result__item-value">{{data.Path}}</span>
+          </div>
+          <div class="result__item">
+            <span class="result__item-label">Size</span>
+            <span class="result__item-value">{{data.Size}}</span>
+          </div>
+          <div class="result__item">
+            <span class="result__item-label">Last used date</span>
+            <span class="result__item-value">{{data.LastModified}}</span>
+          </div>
+        </div>
+      </section>
+      
+    </main>
   </div>
-    
 </template>
 
 <script>
@@ -17,10 +41,11 @@ export default {
   },
   created: async function () {
     this.data = await this.getData()
+    console.log(this.data);
   },
   methods: {
     getData: function () {
-      return fetch('http://c7webtest.azurewebsites.net/searches', {
+      return fetch('http://c7webtest.azurewebsites.net/searches/0/results?start=0&size=5', {
         headers: {
           'content-type': 'application/json'
         }
@@ -32,19 +57,30 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss" scoped>
+.results {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .result {
+    display: flex;
+    flex-direction: column;
+    flex-shrink: 0;
+    padding: 16px;
+    text-align: left;
+    width: 70%;
+    &__item {
+      margin-bottom: 8px;
+      span {
+        display: block;
+      }
+      &-label {
+
+      }
+      &-value {
+
+      }
+    }
+  }
 }
 </style>
